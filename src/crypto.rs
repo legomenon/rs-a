4a3f8078d6bd92ac698a5db4bfca1caa853e4a7e
legomenon <solomovsky.roman@gmail.com>
2023-01-17 14:52:49 +0200

    add crypto functions

---
+++ src/crypto.rs
@@ -0,0 +1,101 @@
+use rand::Rng;
+
+#[derive(Debug)]
+pub struct RSA {
+    pub pub_key: u32,
+    pub priv_key: u32,
+    pub module: u32,
+}
+
+fn discrete_pow(num: u32, exp: u32, module: u32) -> u128 {
+    let mut t: u128 = 1;
+    let mut num: u128 = num as u128;
+    let mut exp: u128 = exp as u128;
+    let module = module as u128;
+
+    while exp > 0 {
+        if exp % 2 != 0 {
+            t = (t * num) % module;
+        }
+        num = (num * num) % module;
+        exp = exp / 2;
+    }
+    t % module
+}
+
+pub fn gen_prime(bits: u32) -> u32 {
+    let mut rng = rand::thread_rng();
+    loop {
+        let num: u32 = rng.gen_range(2u32.pow(bits - 1)..2u32.pow(bits));
+        if is_prime(num) {
+            return num;
+        }
+    }
+}
+
+fn is_prime(n: u32) -> bool {
+    if n < 2 {
+        return false;
+    }
+    if n == 2 || n == 3 {
+        return true;
+    }
+    if n % 2 == 0 || n % 3 == 0 {
+        return false;
+    }
+    let mut i = 5;
+    let mut w = 2;
+    while i * i <= n {
+        if n % i == 0 {
+            return false;
+        }
+        i += w;
+        w = 6 - w;
+    }
+    return true;
+}
+fn multiplicative_inverse(e: u32, fi: u32) -> u32 {
+    let mut k = 1;
+    loop {
+        let result = (1 + (k * fi)) as f64 / e as f64;
+        let rounded = (result * 10_000f64).round() / 10_000f64;
+        if (rounded % 1.0) == 0.0 {
+            return result as u32;
+        } else {
+            k += 1;
+        }
+    }
+}
+
+impl RSA {
+    pub fn new(bits: u32) -> Self {
+        let mut p = gen_prime(bits);
+        let mut q = gen_prime(bits);
+
+        if p < q {
+            let t = p;
+            p = q;
+            q = t;
+        }
+
+        let n = p * q;
+        let fi = (p - 1) * (q - 1);
+        let e = gen_prime(bits - 1);
+        let d = multiplicative_inverse(e, fi);
+
+        Self {
+            pub_key: e,
+            priv_key: d,
+            module: n,
+        }
+    }
+
+    pub fn encrypt(&self, message: String) {
+        let a = message
+            .chars()
+            .filter_map(|c| c.to_digit(10))
+            .map(|c| discrete_pow(c, self.pub_key, self.module))
+            .collect::<Vec<u128>>();
+        println!("{:?}", a)
+    }
+}
--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,14 @@
 use crate::cli::Args;
+use crate::crypto::{gen_prime, RSA};
 use clap::Parser;
 
 pub fn run() {
-    let args = Args::parse();
-    println!("{:?}", args);
+    // let args = Args::parse();
+    // println!("{:?}", args);
+    // let a = gen_prime(26);
+    // let b = gen_prime(26);
+    // println!("{}\n{}", a, b);
+    let rsa = RSA::new(8);
+    rsa.encrypt("hello".to_owned());
+    // println!("{:?}", rsa)
 }
