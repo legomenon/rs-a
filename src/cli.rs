60ca0d90ac61967aebed294037477c9370e3732b
legomenon <solomovsky.roman@gmail.com>
2023-01-19 15:55:15 +0200

    add docs for cli commands

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,7 @@
 use clap::{Parser, Subcommand};
 
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
+#[command(author, version, about)]
 pub struct Args {
     #[command(subcommand)]
     pub command: Option<Commands>,
@@ -9,27 +9,41 @@ pub struct Args {
 
 #[derive(Subcommand, Debug)]
 pub enum Commands {
+    /// generates new RSA key
     Generate {
+        /// output destination
         #[arg(short, long)]
         output: Option<String>,
     },
+
+    /// encrypt provided data
     Encrypt {
+        /// file for encryption
         #[arg(short, long, conflicts_with("message"))]
         file: Option<String>,
+        /// message for encryption
         #[arg(short, long)]
         message: Option<String>,
+        /// output destination
         #[arg(short, long)]
         output: Option<String>,
+        /// rsa key file
         #[arg(short, long)]
         key: String,
     },
+
+    /// decrypt provided data
     Decrypt {
+        /// file for decryption
         #[arg(short, long, conflicts_with("message"))]
         file: Option<String>,
+        /// message for decryption
         #[arg(short, long)]
         message: Option<String>,
+        /// output destination
         #[arg(short, long)]
         output: Option<String>,
+        /// rsa key file
         #[arg(short, long)]
         key: String,
     },
